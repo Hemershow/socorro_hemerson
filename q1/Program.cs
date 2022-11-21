a0b458701e45616279d7d00e39077f9ee21c18e6
Hemershow <61923585+Hemershow@users.noreply.github.com>
2022-11-21 08:39:07 -0300

    Add files via upload

---
+++ q1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+double[] array = new double[]
+{
+    6.6, 7.2, 7.2, 8.4, 8.6, 8.4, 9.4, 9.8, 10.0
+};
+Console.WriteLine(mediaEspecial(array));
+
+double mediaEspecial(double[] array)
+{
+    double[] arrayAux = new double[array.Length];
+
+    array.CopyTo(arrayAux, 0);
+
+    while(arrayAux.Length > 4)
+    {
+        double high = arrayAux.Max();
+        double low = arrayAux.Min();
+
+        var list = arrayAux.ToList();
+
+        list.Remove(high);
+        list.Remove(low);
+
+        arrayAux = list.ToArray();
+    }
+
+    return (arrayAux.Sum()/3);
+}
